41c72f03c67a71e89020125367836427d861d65b
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-12-10 16:17:38 -0300

    fix: subindo alterações após exclusão do hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
 import { PedidoFormComponent } from './pedido-form/pedido-form.component';
 import { FornecedorComponent } from './fornecedor/fornecedor.component';
 
@@ -12,10 +11,8 @@ import { FornecedorComponent } from './fornecedor/fornecedor.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormComponent,
     PedidoFormComponent,
     FornecedorComponent
-
   ],
   imports: [
     BrowserModule,
